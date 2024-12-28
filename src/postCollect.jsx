035c59dc32eebcdde5dd978c9a320a471adbdbbd
Divyansh <svnarayan333@gmail.com>
2024-12-28 12:05:44 +0530

    changes

--- src/postCollect.jsx
+++ src/postCollect.jsx
@@ -8,6 +8,7 @@ export default function Lists(){
                 <Posts imgsrc={"https://images.unsplash.com/photo-1640593967530-7c40ae516b59?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} ownimg={"https://images.unsplash.com/photo-1603775020644-eb8decd79994?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} name={"Vishal"} id={"luky"} likeNo={372}/>
                 <Posts imgsrc={"https://plus.unsplash.com/premium_photo-1731287011777-b3fdfb104dcd?q=80&w=1718&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} ownimg={"https://images.unsplash.com/photo-1731505583021-16c3a17339cd?q=80&w=1665&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} name={"Himanshu"} id={"himan"} likeNo={169}/>
                 <Posts imgsrc={"https://images.unsplash.com/photo-1624804269473-828dcc30a210?q=80&w=1665&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} ownimg={"https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} name={"Rohan"} id={"rohan"} likeNo={123}/>
+                <br /><br /><br /><br />
         </div>
     )
 }
